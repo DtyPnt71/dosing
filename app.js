bab6009460b8eeb22f29f00aa9f017daa72b1b7e
BrnSvr <50246537+DtyPnt71@users.noreply.github.com>
2025-12-16 13:56:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1303,7 +1303,7 @@ async function loadAppVersion() {
   try {
     const res = await fetch('manifest.json', { cache: 'no-store' });
     const manifest = await res.json();
-    const version = manifest.version || "check manifest file!";
+    const version = manifest.version || "1";
     const footer = document.getElementById("appFooter");
     if (footer) {
       footer.textContent = "Version " + version;
